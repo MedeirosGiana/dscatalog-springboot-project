a7570f208bdc71e72c652e9083751a9dbb02335b
giana medeiros <gianamedeiros.00510@gmail.com>
2023-07-05 21:52:05 -0300

    ExceptionHandler

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.services.exception;
+
+public class EntityNotFoundException extends RuntimeException{
+
+    public EntityNotFoundException(String msg) {
+        super(msg);
+    }
+}
